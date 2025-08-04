7b5d35a4cc4982561e3563dd0cb7dfc579d99c12
kittirat17 <kittirat.ngaotham1992@gmail.com>
2025-08-04 11:12:58 +0700

    Create FistIcon.tsx

---
+++ FistIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FistIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg className={className} stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11 21H7a2 2 0 0 1-2-2v-4.59a2 2 0 0 1 .59-1.41l9.82-9.82a2 2 0 0 1 2.83 0l3.58 3.58a2 2 0 0 1 0 2.83L12.41 18.59A2 2 0 0 1 11 19.01V21z"></path>
+        <path d="M7 21v-4"></path>
+        <path d="M21 15h-4"></path>
+    </svg>
+);
